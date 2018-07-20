e15e570a312f1a8bbe264eb8a3c7a4a16d8fdfca
roryjackd <roryjackd@yahoo.com>
2018-07-20 12:35:07 -0600

    RESTful put added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var bodyParser = require("body-parser"),
+methodOverride = require("method-override"),
 mongoose       = require("mongoose"),
 express        = require("express"),
 app            = express();
@@ -8,6 +9,7 @@ mongoose.connect("mongodb://localhost/node_blog");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -61,6 +63,28 @@ app.get("/blogs/:id", function(req, res){
       res.render("show", {blog: foundBlog});
     }
   })
+});
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+      if(err){
+        res.redirect("/blogs");
+      } else {
+        res.render("edit", {blog: foundBlog});
+      }
+    });
+  });
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+    if(err){
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs/" + req.params.id);
+    }
+  });
 });
 
 app.listen(3000, () => console.log('App listening on port 3000!'))
