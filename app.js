c6d98245a11096d6f81c85737f530ab91c72fa3b
roryjackd <roryjackd@yahoo.com>
2018-07-19 21:05:50 -0600

    RESTful new and create added

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.get("/", function(req, res){
   res.redirect("/blogs");
 });
 
+// INDEX ROUTE
 app.get("/blogs", function(req, res){
     Blog.find({}, function(err, blogs){ 
       if(err){
@@ -35,4 +36,20 @@ app.get("/blogs", function(req, res){
       }
   });
 });
+
+// NEW ROUTE
+app.get("/blogs/new", function(req, res){
+  res.render("new");
+});
+// CREATE ROUTE
+app.post("/blogs", function(req, res){
+  Blog.create(req.body.blog, function(err, newBlog){
+    if(err){
+      res.render("new");
+    } else {
+      res.redirect("/blogs");
+    }
+  });
+});
+
 app.listen(3000, () => console.log('App listening on port 3000!'))
