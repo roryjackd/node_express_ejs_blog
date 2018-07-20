6a59d5854b7390fd37bda0e8896f4fac259a0c33
roryjackd <roryjackd@yahoo.com>
2018-07-20 17:31:56 -0600

    express-sanitizer added home page styled

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var bodyParser = require("body-parser"),
 methodOverride = require("method-override"),
+expressSanitizer = require("express-sanitizer"),
 mongoose       = require("mongoose"),
 express        = require("express"),
 app            = express();
@@ -9,6 +10,7 @@ mongoose.connect("mongodb://localhost/node_blog");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
@@ -45,6 +47,7 @@ app.get("/blogs/new", function(req, res){
 });
 // CREATE ROUTE
 app.post("/blogs", function(req, res){
+  req.body.blog.body = req.sanitize(req.body.blog.body)
   Blog.create(req.body.blog, function(err, newBlog){
     if(err){
       res.render("new");
@@ -78,6 +81,7 @@ app.get("/blogs/:id/edit", function(req, res){
 
 // UPDATE ROUTE
 app.put("/blogs/:id", function(req, res){
+  req.body.blog.body = req.sanitize(req.body.blog.body)
   Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
     if(err){
       res.redirect("/blogs");
