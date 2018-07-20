92f7839fccf0df5faa7ec09a8247044742e3a1d5
roryjackd <roryjackd@yahoo.com>
2018-07-20 13:49:08 -0600

    RESTful destroy added

--- app.js
+++ app.js
@@ -87,6 +87,17 @@ app.put("/blogs/:id", function(req, res){
   });
 });
 
+// DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+  Blog.findByIdAndRemove(req.params.id, function(err){
+    if(err){
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs");
+    }
+  })
+});
+
 app.listen(3000, () => console.log('App listening on port 3000!'))
 
 
